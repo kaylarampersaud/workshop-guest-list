468392ba421f7a241ffe0590c817219c2f6f75c4
Kayla Rampersaud <krampersaud98@gmail.com>
2026-02-09 11:36:44 -0500

    add to app.jsx and created guestDetails.jsx and guestList.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import { useState } from "react";
+
+import GuestDetails from "./guestDetails";
+import GuestList from "./guestList";
+
+/** Switch between guests & details of a single guest */
 export default function App() {
-  return <></>;
+  const [guestId, setGuestId] = useState(null);
+  return (
+    <main>
+      {guestId ? (
+        <GuestDetails guestId={guestId} setGuestId={setGuestId} />
+      ) : (
+        <GuestList setGuestId={setGuestId} />
+      )}
+    </main>
+  );
 }
